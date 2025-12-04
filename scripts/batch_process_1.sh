4b18951ee040fd1132352399063a62f0b53e672a
kailashjagadeesh <kailashjagadeesh2000@gmail.com>
2025-12-04 02:06:54 +0000

    dummy push

---
+++ scripts/batch_process_1.sh
@@ -0,0 +1,2 @@
+bash scripts/train_policy.sh dp3_dino metaworld_dial-turn_dino metaworld_dial-turn_dino_high_res 0 0
+bash scripts/train_policy.sh dp3_dino metaworld_hammer_dino metaworld_hammer_dino_high_res 0 0
---
+++ scripts/batch_process_all.sh
@@ -0,0 +1,18 @@
+#basketball
+bash scripts/train_policy.sh dp3_dino metaworld_basketball_dino_128 metaworld_basketball_dino_low_res 0 0
+
+# dial-turn
+bash scripts/train_policy.sh dp3_dino metaworld_dial-turn_dino_128 metaworld_dial-turn_dino_low_res 0 0
+bash scripts/train_policy.sh dp3_dino metaworld_dial-turn_dino metaworld_dial-turn_dino_high_res 0 0
+
+# hammer
+bash scripts/train_policy.sh dp3_dino metaworld_hammer_dino_128 metaworld_hammer_dino_low_res 0 0
+bash scripts/train_policy.sh dp3_dino metaworld_hammer_dino metaworld_hammer_dino_high_res 0 0
+
+# shelf-place
+bash scripts/train_policy.sh dp3_dino metaworld_shelf-place_dino_128 metaworld_shelf-place_dino_low_res 0 0
+bash scripts/train_policy.sh dp3_dino metaworld_shelf-place_dino metaworld_shelf-place_dino_high_res 0 0
+
+# sweep-into
+bash scripts/train_policy.sh dp3_dino metaworld_sweep-into_dino_128 metaworld_sweep-into_dino_low_res 0 0
+bash scripts/train_policy.sh dp3_dino metaworld_sweep-into_dino metaworld_sweep-into_dino_high_res 0 0
